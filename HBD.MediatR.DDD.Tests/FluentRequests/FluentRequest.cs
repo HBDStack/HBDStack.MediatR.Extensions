9b61e91487e5b6a3b08761c895587a0a5991407b
Steven Hoang <hoangbaoduy@gmail.com>
2022-10-16 11:19:39 +0800

    Update FluentRequest.cs

--- HBD.MediatR.DDD.Tests/FluentRequests/FluentRequest.cs
+++ HBD.MediatR.DDD.Tests/FluentRequests/FluentRequest.cs
@@ -16,9 +16,5 @@ public record FluentResponse
 internal class FluentRequestHandler : IRequestFluentHandler<FluentRequest, FluentResponse>
 {
     public async Task<IResult<FluentResponse>> Handle(FluentRequest request, CancellationToken cancellationToken)
-    {
-        if (string.IsNullOrEmpty(request.Name))
-            return Result.Fails<FluentResponse>("The name is invalid.");
-        return Result.Ok(new FluentResponse());
-    }
+        => string.IsNullOrEmpty(request.Name) ? Result.Fails<FluentResponse>("The name is invalid.") : Result.Ok(new FluentResponse());
 }
